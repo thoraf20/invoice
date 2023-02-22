aa7774a009e2874053e386063f5b7f7ce485a2bf
Toheeb Rauf <thoraf20@gmail.com>
2023-02-22 22:18:42 +0100

    auth module created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
+const corsOrigin = '*';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const corsOptions = {
+    origin: corsOrigin,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
+  const app = await NestFactory.create(AppModule, { cors: corsOptions });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.use(helmet());
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Logger } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
+
+  constructor(private readonly authService: AuthService) {}
+}
